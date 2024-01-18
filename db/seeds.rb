f22e779d65b22d565ee6cf3e6ffa6385f733122c
Marco Castro <marcodotcastro@gmail.com>
2024-01-18 10:29:51 -0300

    feat: add user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 if Rails.env.development?
   AdminUser.create!(email: 'admin@mail.com',
                     password: 'password', password_confirmation: 'password')
+  User.create!(email: 'admin@limpar.com',
+                    password: 'password', password_confirmation: 'password')
   goal1 = Goal.create(name: 'Aprender Linguagem Ruby',
                       description: 'Quero criar 10 algoritmos em até 3 meses', status: 'done')
   Task.create(name: '1ª agoritmo', description: 'Criar o algoritmo bubble sort',
