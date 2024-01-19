907c4b01e5a565de32eba1b509bc5ad1805ce550
Cassio Santana <cassioo.roger@gmail.com>
2024-01-19 06:48:04 -0300

    test(client): add model tests
    Ref #7

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -10,5 +10,16 @@
 require 'rails_helper'
 
 RSpec.describe Client, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+  end
+
+  describe '#to_s' do
+    let(:client) { create(:client) }
+
+    it 'return the client name' do
+      expect(client.to_s).to eq(client.name)
+    end
+  end
 end
