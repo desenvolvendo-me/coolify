cfd267ca783348cdbab923913d43a0779b45969b
Marco Castro <marcodotcastro@gmail.com>
2024-01-23 16:38:47 -0300

    feat(user): add reset password

--- app/controllers/manager/users_controller.rb
+++ app/controllers/manager/users_controller.rb
@@ -18,7 +18,9 @@ module Manager
 
     def create
       @user = User.new(user_params)
-      if @user.save
+      @user.confirm
+      if @user.save(validate: false)
+        @user.send_reset_password_instructions
         redirect_to manager_user_path(@user),
                     notice: t('.create')
       else
