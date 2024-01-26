0de719b586c16f0d6f01fbe6005d4276ebb57212
Marco Castro <marcodotcastro@gmail.com>
2024-01-26 09:34:20 -0300

    refactor: seeds

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,8 @@ module StartupMissionBaseRails
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.autoload_paths += %W(#{config.root}/lib)
+
   end
 end
