e25dbda3d8a723b3cc22141c5015b4f9db1238fe
Philipe Rodrigues <dev.philipe@outlook.com>
2024-02-05 15:04:58 -0300

    chore(database): Removes migrations

--- db/migrate/20240131165630_add_company_to_clients.rb
+++
@@ -1,5 +0,0 @@
-class AddCompanyToClients < ActiveRecord::Migration[7.0]
-  def change
-    add_column :clients, :company_id, :integer
-  end
-end
--- db/migrate/20240201180311_rename_equipment_to_coolers.rb
+++
@@ -1,5 +0,0 @@
-class RenameEquipmentToCoolers < ActiveRecord::Migration[7.0]
-  def change
-    rename_table :equipment, :coolers
-  end
-end
