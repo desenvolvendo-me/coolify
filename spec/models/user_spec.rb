2bef17c8df3eb8940438905f0590ddb203b942d7
Cassio Santana <cassioo.roger@gmail.com>
2024-02-05 09:57:38 -0300

    test(user): add tests that check the cft field
    Ref #39

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,7 @@
 #
 #  id                     :bigint           not null, primary key
 #  avatar                 :string
+#  cft                    :string
 #  confirmation_sent_at   :datetime
 #  confirmation_token     :string
 #  confirmed_at           :datetime
@@ -29,5 +30,26 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'associations' do
     it { should belong_to(:company) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+
+    context 'for technical leads' do
+      let(:technical_lead) { build(:user, role: 'technical_lead', cft: nil) }
+
+      it 'requires CFT for technical leads' do
+        technical_lead.valid?
+        expect(technical_lead.errors[:cft]).to include(I18n.t('activerecord.errors.models.user.attributes.cft.blank'))
+      end
+    end
+
+    context 'for other roles' do
+      let(:admin) { build(:user, role: 'admin', cft: nil) }
+
+      it 'does not require CFT for other roles' do
+        expect(admin).to be_valid
+      end
+    end
   end
 end
