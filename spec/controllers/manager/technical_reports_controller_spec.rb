82865d600da036de727f57507da663c6f982934b
Philipe Rodrigues <dev.philipe@outlook.com>
2024-02-06 14:39:40 -0300

    test(technical_report): Create RSpec controller tests

--- spec/controllers/manager/technical_reports_controller_spec.rb
+++ spec/controllers/manager/technical_reports_controller_spec.rb
@@ -2,16 +2,29 @@ require 'rails_helper'
 
 RSpec.describe Manager::TechnicalReportsController, type: :controller do
   let(:admin_user) { create(:user, role: :admin) }
+  let(:company) { create(:company) }
+  let(:client) { create(:client, company: company) }
+
   before do
     sign_in admin_user
     ActsAsTenant.current_tenant = admin_user.company
   end
 
-  describe 'GET #new' do
-    it 'assigns a new technical_report as @technical_report' do
-      get :new
-      expect(assigns(:technical_report)).to be_a_new(TechnicalReport)
+  describe 'POST #create' do
+    it 'creates a new technical report' do
+      expect do
+        post :create, params: { client_id: client.id }
+      end.to change(TechnicalReport, :count).by(1)
+
+      expect(response).to redirect_to(manager_technical_report_path(TechnicalReport.last))
+    end
+
+    it 'redirects to manager_clients_path on failure' do
+      allow_any_instance_of(TechnicalReport).to receive(:save).and_return(false)
+
+      post :create, params: { client_id: client.id }
+
+      expect(response).to redirect_to(manager_clients_path)
     end
   end
-
 end
---
+++ spec/factories/technical_report.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :technical_report do
+    client
+    company
+  end
+end
