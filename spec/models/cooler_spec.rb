f74ad06bc05fe362fb2b0cad4a2e75d06a5ffba3
Marco Castro <marcodotcastro@gmail.com>
2024-02-06 11:45:35 -0300

    test: add client to cooler

--- spec/models/cooler_spec.rb
+++ spec/models/cooler_spec.rb
@@ -13,6 +13,7 @@ require 'rails_helper'
 RSpec.describe Cooler, type: :model do
   describe 'association' do
     it { should belong_to(:company) }
+    it { should belong_to(:client) }
     it { should have_many(:maintenances) }
   end
 
