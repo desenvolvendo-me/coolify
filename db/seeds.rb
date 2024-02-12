63a59310e12838186ba9dc1a0a1df47c85c4f99c
Cassio Santana <cassioo.roger@gmail.com>
2024-02-12 13:57:04 -0300

    fix(user_creator): add cft rule
    Ref #51

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,10 @@ if Rails.env.development?
   company_2 = Company.create(cnpj: FFaker::IdentificationBR.cnpj)
 
   # Users
-  UserCreator.create_user('Administrador 1', 'admin1@limpar.com', :admin, company_1, 'avatar-1.jpg')
+  UserCreator.create_user('Administrador 1', 'admin1@limpar.com', :admin, company_1, avatar_file: 'avatar-2.jpg')
   UserCreator.create_user('Administrador 1', 'admin2@limpar.com', :admin, company_2)
-  UserCreator.create_user('Funcionário 1', 'employee1@limpar.com', :employee, company_1, 'avatar-2.jpg')
-  UserCreator.create_user('Funcionário 2', 'employee2@limpar.com', :technical_lead, company_1, 'avatar-2.jpg')
+  UserCreator.create_user('Funcionário 1', 'employee1@limpar.com', :employee, company_1, avatar_file: 'avatar-2.jpg')
+  UserCreator.create_user('Funcionário 2', 'employee2@limpar.com', :technical_lead, company_1, avatar_file: 'avatar-2.jpg', cft: FFaker::Lorem.characters(10))
 
   # Clients
   10.times do |i|
--- spec/models/cooler_spec.rb
+++ spec/models/cooler_spec.rb
@@ -1,12 +1,21 @@
 # == Schema Information
 #
-# Table name: cooler
+# Table name: coolers
 #
 #  id         :bigint           not null, primary key
 #  tag        :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  client_id  :integer
 #  company_id :integer
+#
+# Indexes
+#
+#  index_coolers_on_client_id  (client_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (client_id => clients.id)
 #
 require 'rails_helper'
 
