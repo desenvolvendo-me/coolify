463495f082506011b71e16660326e1a62a67f4f3
Cassio Santana <cassioo.roger@gmail.com>
2024-03-05 17:40:39 -0300

    test(Linker): adds test of the link between maintenance and plan
    Ref #55

---
+++ spec/business/linker_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Maintenances::Linker, type: :business do
+  describe '#call when a to_do plan exists' do
+    let(:company) { create(:company) }
+    let(:maintenance) { create(:maintenance, company: company) }
+
+    subject { described_class.call(maintenance) }
+
+    context 'when a to_do plan exists' do
+      let!(:to_do_plan) { create(:maintenance_plan, status: :to_do, company: company) }
+
+      it 'links the maintenance to the to_do plan' do
+        subject
+        expect(maintenance.reload.maintenance_plan).to eq(to_do_plan)
+      end
+
+      it 'returns true for success?' do
+        expect(subject).to be_truthy
+      end
+    end
+  end
+end
